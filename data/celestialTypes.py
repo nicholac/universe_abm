76c775b4a9da514cea9cc2e76c91d2b964105cd1
nicholac <kris_nich@hotmail.co.uk>
2016-12-27 12:22:04 +0000

    init commit - explorers working, universe builds

---
+++ data/celestialTypes.py
@@ -0,0 +1,22 @@
+'''
+Created on 25 Dec 2016
+
+@author: dusted-ipro
+
+Data about the types of celestials
+
+'''
+
+
+def planets():
+    return {0:'rocky',
+            1:'ocean',
+            2:'ice',
+            3:'gas',}
+
+
+def stars():
+    return {0:'red',
+            1:'dwarf',
+            2:'pulsar',
+            3:'blue',}
---
+++ data/tradableTypes.py
@@ -0,0 +1,27 @@
+'''
+Created on 25 Dec 2016
+
+@author: dusted-ipro
+
+Contains callable data structures for all good types
+
+'''
+
+def allTradables():
+    '''
+    ::returns Dict All tradable goods
+    '''
+    return {0:'Harvestor Goods',
+            1:'Trader Goods',
+            2:'Fabricator Goods',
+            3:'Criminal Goods',
+            4:'Military Goods',
+            5:'General Goods',
+            6:'Explorer Goods',
+            7:'Energy',
+            8:'Raw Materials',
+            9:'Energy Coords',
+            10:'Raw Material Coords'}
+
+
+
